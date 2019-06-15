8745d7a4fb49babdddac261d99fcd97e9cd7265b
Yukiya Nakagawa <yn.airscope@gmail.com>
2019-06-15 19:14:24 +0900

    フッター消した

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,6 @@
 </template>
 
 <script>
-import Footer from './components/Footer.vue'
 import Questions from './components/Questions'
 import Result from './components/Result'
 import {parse} from 'querystring'
